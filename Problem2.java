1919685c3426b098042004d1ef8003e1328e7912
vivekadi <vivek.aditya@abzooba.com>
2015-08-17 12:08:39 +0530

    Done Project

--- Problem2.java
+++ Problem2.java
@@ -1,9 +1,8 @@
-package problem2;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
-
-import java.util.*;
-import java.io.*;
-import java.lang.*;
 /**
  * @author vikash kumawat
  *
@@ -11,45 +10,40 @@ import java.lang.*;
 public class Problem2 {
 
 	public static void main(String[] args) {
-		Chat newChat=new Chat();
+		Chat newChat = new Chat();
 		new T1(newChat);
 		new T2(newChat);
-				
 	}
-
 }
 
+class Chat {
 
- class Chat {
-	
 	boolean flag = false;
-	
-	public synchronized void Question (String msg){
-		if(flag) {
-			try{
+
+	public synchronized void Question(String msg) {
+		if (flag) {
+			try {
 				wait();
-			} catch(InterruptedException e) {
+			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
-		
 		System.out.println(msg);
 		flag = true;
 		notify();
-		
+
 	}
-	
-	public synchronized void Answer (String msg) {
+
+	public synchronized void Answer(String msg) {
 		if (!flag) {
 			try {
 				wait();
-				
-			} catch(InterruptedException e) {
+
+			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-			
+
 		}
-		
 		System.out.println(msg);
 		flag = false;
 		notify();
@@ -57,67 +51,54 @@ public class Problem2 {
 
 }
 
-
- class T1 implements Runnable {
+class T1 implements Runnable {
 	Chat m;
-	
-	public T1 (Chat m1){
-		
+
+	public T1(Chat m1) {
+
 		this.m = m1;
-		new Thread (this,"Question").start();
-		
+		new Thread(this, "Question").start();
+
 	}
 
 	public void run() {
-		 
-		try(BufferedReader br= new BufferedReader(new FileReader("C:\\Users\\vikash.kumawat\\workspace\\13AugThread\\src\\p022_names1.txt")))  {
-		    String line1;
-		    while ((line1 = br.readLine()) != null) {
-		       m.Question(line1);
-		    	}
-		    br.close();
-		    
-		}catch(FileNotFoundException exception){
+
+		try (BufferedReader br = new BufferedReader(new FileReader("p022_names1.txt"))) {
+			String line1;
+			while ((line1 = br.readLine()) != null) {
+				m.Question(line1);
+			}
+			br.close();
+
+		} catch (FileNotFoundException exception) {
 			System.out.println("File not found\n");
-		}
-		catch(IOException exception){
+		} catch (IOException exception) {
 			System.out.println(exception);
 		}
-		
-		
-		
 	}
-
 }
-
 
 class T2 implements Runnable {
 	Chat m;
-	public T2 (Chat m2){
-		
+
+	public T2(Chat m2) {
 		this.m = m2;
-		new Thread (this,"Answer").start();
+		new Thread(this, "Answer").start();
 	}
 
 	public void run() {
-		
-		 
-		try(BufferedReader br2= new BufferedReader(new FileReader("C:\\Users\\vikash.kumawat\\workspace\\13AugThread\\src\\p081_matrix1.txt")))  {
-		    String line;
-		    while ((line = br2.readLine()) != null) {
-		    	m.Answer(line);
-		    }
-		    br2.close();
-		    
-		}catch(FileNotFoundException exception){
+
+		try (BufferedReader br2 = new BufferedReader(new FileReader("p081_matrix1.txt"))) {
+			String line;
+			while ((line = br2.readLine()) != null) {
+				m.Answer(line);
+			}
+			br2.close();
+
+		} catch (FileNotFoundException exception) {
 			System.out.println("File not found\n");
-		}
-		catch(IOException exception){
+		} catch (IOException exception) {
 			System.out.println(exception);
 		}
-		
-		
-		
 	}
-
 }
--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 Group 4 Repo
 
 Problem 1 Done 
-Waiting for Problem 2 to be pushed to the repo
+Problem 2 Done
